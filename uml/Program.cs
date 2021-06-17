45f00cf10c8d1364f7cd559ff3780421155f3616
GIMENEZ69 <82936639+GIMENEZ69@users.noreply.github.com>
2021-06-17 15:21:20 +0200

    Add files via upload

---
+++ uml/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
