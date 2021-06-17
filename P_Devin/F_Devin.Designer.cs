705f3708f1212243e800bf328d71fe1205f6c3cc
GIMENEZ69 <82936639+GIMENEZ69@users.noreply.github.com>
2021-06-17 15:22:39 +0200

    Add files via upload

---
+++ P_Devin/F_Devin.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace P_Devin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CmdOK = new System.Windows.Forms.Button();
+            this.CmdInit = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Banner", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Saisir un nombre entre 1 et 9999";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(395, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(2, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // CmdOK
+            // 
+            this.CmdOK.Location = new System.Drawing.Point(534, 40);
+            this.CmdOK.Name = "CmdOK";
+            this.CmdOK.Size = new System.Drawing.Size(53, 22);
+            this.CmdOK.TabIndex = 0;
+            this.CmdOK.Text = "OK";
+            this.CmdOK.UseVisualStyleBackColor = true;
+            this.CmdOK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CmdInit
+            // 
+            this.CmdInit.Enabled = false;
+            this.CmdInit.Location = new System.Drawing.Point(634, 27);
+            this.CmdInit.Name = "CmdInit";
+            this.CmdInit.Size = new System.Drawing.Size(110, 39);
+            this.CmdInit.TabIndex = 0;
+            this.CmdInit.Text = "Retirage";
+            this.CmdInit.UseVisualStyleBackColor = true;
+            this.CmdInit.Click += new System.EventHandler(this.CmdInit_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(360, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(26, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(286, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(26, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(26, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(286, 31);
+            this.label4.TabIndex = 5;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(26, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(286, 31);
+            this.label5.TabIndex = 6;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(26, 244);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(286, 31);
+            this.label6.TabIndex = 7;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(26, 275);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(286, 31);
+            this.label7.TabIndex = 8;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(392, 120);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(286, 31);
+            this.label8.TabIndex = 9;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Location = new System.Drawing.Point(392, 151);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(286, 31);
+            this.label9.TabIndex = 10;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Location = new System.Drawing.Point(392, 182);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(286, 31);
+            this.label10.TabIndex = 11;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Location = new System.Drawing.Point(392, 213);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(286, 31);
+            this.label11.TabIndex = 12;
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label12.Location = new System.Drawing.Point(392, 244);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(286, 31);
+            this.label12.TabIndex = 13;
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label13.Location = new System.Drawing.Point(392, 275);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(286, 31);
+            this.label13.TabIndex = 14;
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(271, 367);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 45);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Terminer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(26, 396);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 13);
+            this.label14.TabIndex = 16;
+            this.label14.Text = "label14";
+            this.label14.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.CmdInit);
+            this.Controls.Add(this.CmdOK);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Devinez un nombre";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button CmdOK;
+        private System.Windows.Forms.Button CmdInit;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ P_Devin/F_Devin.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P_Devin
+{
+    public partial class Form1 : Form
+    {
+        public int MonTirage;
+        public int NbTour = 0;
+
+        static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        string Resultat(string MonNombre)
+        {
+            int MonNombre2 = int.Parse(MonNombre);
+            string MaChaine = "";
+            if (MonNombre2< MonTirage)
+            {
+                MaChaine = MonNombre + " est inférieur au tirage";
+            }
+            else if (MonNombre2>MonTirage)
+            {
+                MaChaine = MonNombre + " est supérieur au tirage";
+            }
+            else
+            {
+                MaChaine = " Gagné, vous avez trouvé";
+                CmdOK.Enabled = false;
+                CmdInit.Enabled = true;
+            
+            }
+            return MaChaine;
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MonTirage = RandomNumber(1, 9999);
+            label14.Text = Convert.ToString(MonTirage);
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            // Controle de saisi
+            if (textBox2.Text=="")
+            {
+                MessageBox.Show("Vous devez saisir un nombre entre 1 et 9999 !!!");
+                textBox2.Focus();
+                return;
+            }
+            else if (!Int32.TryParse(textBox2.Text,out n))
+            {
+                MessageBox.Show("Vous devez saisir un nombre entre 1 et 9999 !!!");
+                textBox2.Focus();
+                return;
+            }
+            else if(int.Parse(textBox2.Text)< 1 || int.Parse(textBox2.Text)> 9999)
+            {
+                MessageBox.Show("Vous devez saisir un nombre entre 1 et 9999 !!!");
+                textBox2.Focus();
+                return;
+            }
+
+
+            NbTour++;
+            switch(NbTour)
+            {
+                case 1:
+                    label2.Text = Resultat(textBox2.Text);
+                    break;
+                case 2:
+                    label3.Text = Resultat(textBox2.Text);
+                    break;
+                case 3:
+                    label4.Text = Resultat(textBox2.Text);
+                    break;
+                case 4:
+                    label5.Text = Resultat(textBox2.Text);
+                    break;
+                case 5:
+                    label6.Text = Resultat(textBox2.Text);
+                    break;
+                case 6:
+                    label7.Text = Resultat(textBox2.Text);
+                    break;
+                case 7:
+                    label8.Text = Resultat(textBox2.Text);
+                    break;
+                case 8:
+                    label9.Text = Resultat(textBox2.Text);
+                    break;
+                case 9:
+                    label10.Text = Resultat(textBox2.Text);
+                    break;
+                case 10:
+                    label11.Text = Resultat(textBox2.Text);
+                    break;
+                case 11:
+                    label12.Text = Resultat(textBox2.Text);
+                    break;
+                case 12:
+                    label13.Text = Resultat(textBox2.Text);
+                    break;
+            }
+            textBox2.Text ="";
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form.ActiveForm.Close();
+        }
+
+        private void CmdInit_Click(object sender, EventArgs e)
+        {
+            MonTirage = RandomNumber(1, 9999);
+            label14.Text = Convert.ToString(MonTirage);
+            CmdInit.Enabled = false;
+            CmdOK.Enabled = true;
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+            label10.Text = "";
+            label11.Text = "";
+            label12.Text = "";
+            label13.Text = "";
+            NbTour = 0;
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
