ac455b2da4bf8bb3b2d56c98e7954d49a567c31e
GIMENEZ69 <82936639+GIMENEZ69@users.noreply.github.com>
2021-06-17 15:28:59 +0200

    Add files via upload

---
+++ Tuto_Exo/Program.cs
@@ -0,0 +1,7 @@
+﻿// déclaration et instanciation d'un nouvel objet en utilisant son constructeur
+CompteBancaire comptePierre = new CompteBancaire("Pierre", 0, "euros");
+
+// appels de méthodes
+comptePierre.Crediter(300);
+comptePierre.Debiter(500);
+Console.WriteLine(comptePierre.Decrire());
